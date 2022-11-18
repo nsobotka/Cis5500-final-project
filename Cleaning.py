bafa444d0e91e78c3207f5961f62756f54bca735
nsobotka <90986529+nsobotka@users.noreply.github.com>
2022-11-17 20:53:17 -0500

    starting to parse songs

--- Cleaning.py
+++ Cleaning.py
@@ -20,3 +20,56 @@ def genres():
     genres.to_csv('genres_processed.csv', index=False)
 
 # genres()
+
+def songs():
+    col_names = ['id', 'name', 'album', 'album_id', 'artists', 'artist_ids',
+                'track_number', 'disk_number', 'explicit', 'danceability', 
+                'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+                'instrumentalness', 'liveness', 'valence', 'tempo',
+                'duration_ms', 'time_signature', 'year', 'release_date']
+    songs = pd.read_csv('tracks_features.csv', header=None, names=col_names, low_memory=False)
+    songs = songs.drop([0])
+    songs = songs.drop(columns=['year', 'track_number', 'disk_number'])
+
+    songsNew = pd.DataFrame(columns = ['id', 'name', 'album', 'album_id', 'artists', 'artist_ids',
+                'explicit', 'danceability', 
+                'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+                'instrumentalness', 'liveness', 'valence', 'tempo',
+                'duration_ms', 'time_signature', 'release_date'])
+
+    count = 0
+    for i in songs.index:
+        print(i)
+        print(songs.loc[[i]])
+        print(songs['id'][i], songs['name'][i])
+        length = len(songs['artists'][i].split(","))
+        if length > 1:
+            print(songs['artists'][i].split(","))
+            # add a new row per artist name
+            # songsNew = songsNew.append(songs[i])
+        else: 
+            songsNew = songsNew.append(songs.loc[[i]])
+        #     continue
+        
+        count = count + 1
+        if count > 4:
+            break
+
+    # print(songs.head())
+    print(songsNew.head())
+    pass
+
+songs()
+
+def artists():
+    pass
+
+def album_info():
+    pass
+
+
+
+def charts():
+    # This is complicated because we want to add song_id and artist_id from the previous two tables into this table.
+    pass
+
