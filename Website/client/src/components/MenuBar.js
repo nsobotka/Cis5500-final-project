c4ae4c896b00c2caa31bde9f88bda055e6714333
nsobotka <90986529+nsobotka@users.noreply.github.com>
2022-12-03 18:37:18 -0500

    missing files

---
+++ Website/client/src/components/MenuBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from "shards-react";
+
+class MenuBar extends React.Component {
+    render() {
+        return (
+            <Navbar type="dark" theme="primary" expand="md">
+                <NavbarBrand href="/">CIS 550 Spotify</NavbarBrand>
+                <Nav navbar>
+                    <NavItem>
+                        <NavLink active href="/">
+                            Home
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink active href="/artists">
+                            Artists
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink active href="/songs">
+                            Songs
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        )
+    }
+}
+
+export default MenuBar
